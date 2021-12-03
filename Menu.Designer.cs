8d4f0f2fc3f2c30bd0147baa5e50a5a3966e8947
TBoeren <thomasboeren@outlook.com>
2021-12-03 16:18:55 +0100

    Added icon for the application

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -2116,6 +2116,7 @@
             this.ClientSize = new System.Drawing.Size(1247, 650);
             this.Controls.Add(this.WorldMapPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Menu";
             this.Text = "Sherlock Holmes Consulting Detective";
             this.Load += new System.EventHandler(this.Form1_Load);
